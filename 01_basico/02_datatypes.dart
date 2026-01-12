72580d08c553eefb73fee2e68a7244b6e48b1de9
ruminahuelwalter <ruminahuelwalter@gmail.com>
2026-01-11 22:36:58 -0300

    add: números

---
+++ 01_basico/02_datatypes.dart
@@ -0,0 +1,18 @@
+main(){
+  // Números
+  int a = 10;
+
+  double b = 5.5;
+
+  int? c;
+
+  int _a = 30;
+  double $b = 40;
+  double resultado =_a + $b;
+
+  print(resultado);
+  print(a);
+  print(b);
+  print(c);
+
+}
